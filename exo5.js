928abf1329cca7787a71ccc5dbdb67ad2dfa5330
Thomas.D <d.thomas@semifir.com>
2021-10-19 11:02:08 +0200

    update exo5

--- exo5.js
+++ exo5.js
@@ -2,16 +2,16 @@ let read = require("readline-sync");
 
 let note = read.questionFloat("Saisir note : ")
 
-if(note > 0 && note <= 4) {
-    console.log("Catastrophique")
-} else if(note > 0 && note <= 10) {
-    console.log("Insuffisant")
-} else if(note > 0 && note <= 14) {
-    console.log("Passable")
-} else if(note > 0 && note <= 18) {
-    console.log("Bien")
-} else if(note > 0 && note <= 20) {
-    console.log("Très bien")
-} else {
+if(note < 0 || note > 20) {
     console.log("Note invalide")
+} else if(note >= 0 && note <= 4) {
+    console.log("Catastrophique")
+} else if(note <= 10) {
+    console.log("Insuffisant")
+} else if(note <= 14) {
+    console.log("Passable")
+} else if(note <= 18) {
+    console.log("Bien")
+} else if(note <= 20) {
+    console.log("Très bien")
 }
